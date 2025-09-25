c1d61fdb67f12e747558b009aec11b7a9a16b4e4
Nanba1Hiro <126610871+Nanba1Hiro@users.noreply.github.com>
2025-09-25 14:37:17 +0200

    launch camera variable that handles the image captured

--- MOBILE_APP_CODE/MyApplication3/app/src/main/java/com/example/myapplication/ImagesInfo.java
+++ MOBILE_APP_CODE/MyApplication3/app/src/main/java/com/example/myapplication/ImagesInfo.java
@@ -1,17 +1,52 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ImagesInfo extends AppCompatActivity {
+    private ImageView imageView;
+    private ActivityResultLauncher<Intent> cameraLauncher;
+    private ActivityResultLauncher<Intent> galleryLauncher;
+    private ActivityResultLauncher<String> requestPermissionLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_collection);
+
+        imageView = findViewById(R.id.imageView);
+        Button cameraButton = findViewById(R.id.button_camera);
+        Button galleryButton = findViewById(R.id.button_gallery);
+
+        // --- Result Launchers ---
+        // Handles the result from the camera
+        cameraLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                        Bundle extras = result.getData().getExtras();
+                        Bitmap imageBitmap = (Bitmap) extras.get("data");
+                        imageView.setImageBitmap(imageBitmap); //-> displays the bitmap captured
+                        // Later, we will convert this bitmap to JSON data
+                    }
+                });
+
     }
 }
