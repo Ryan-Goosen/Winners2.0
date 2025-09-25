6b838d2e03eaf990f2db361e3a38a64f71e3f0cc
Nanba1Hiro <126610871+Nanba1Hiro@users.noreply.github.com>
2025-09-25 12:27:56 +0200

    Currently creating button functionality of our report button to enable the user to process their request

---
+++ MOBILE_APP_CODE/MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.app_layout); //-> linking to app_layout.xml
+
+        Button reportIssueButton = findViewById(R.id.report_button); //-> finding the report button in the layout
+
+        reportIssueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button Clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
